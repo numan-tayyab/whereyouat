923113732d33e8f856ae2cd9b2a91e9ce110144b
Numan T <numan.tayyab@ilsainteractive.com>
2014-01-08 15:07:18 +0500

    assets compress issue fixing on heroku

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Whereyouat
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(bootstrap-datetimepicker.css)
+    config.assets.precompile += %w(bootstrap-colorpicker.js)
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,11 @@ Whereyouat::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
